56d54246f49bf921d01ae30ef4c0840f2c1b1e85
Dario Guarascio <dario.guarascio@gmail.com>
2023-05-26 14:27:53 +0200

    added template listing page

---
+++ astro/src/models/listing.ts
@@ -0,0 +1,35 @@
+import config from "@utils/config.js";
+import directus from "@utils/directus.js";
+
+export default async function() {
+  try {
+    let query = await directus.items('report').readByQuery({
+      "limit": 10,
+      "filter": { },
+      "fields": [
+        "id",
+        "status",
+        "location",
+        "theft_date",
+        "colors",
+        "approximate_value",
+        "approximate_value_currency",
+        "bike_brand_model.name",
+        "bike_brand_model.bike_brand.name",
+        "bike_brand_model.bike_brand.key",
+        "main_photo.id",
+        "main_photo.filename_disk",
+        "main_photo.type",
+        "main_photo.width",
+        "main_photo.height"
+      ]
+    });
+    let data = query && query.data ? query.data : []
+
+    return (!data || data.length == 0) ? false : data;
+
+  } catch (e) {
+    throw e
+  }
+
+}
