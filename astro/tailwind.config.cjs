e87c12cfab9cc6e8bd76f99b613f3417d25b590b
Dario Guarascio <dario.guarascio@gmail.com>
2023-05-26 14:28:22 +0200

    fixed indentation to space=2

--- astro/tailwind.config.cjs
+++ astro/tailwind.config.cjs
@@ -4,25 +4,25 @@ const plugin = require('tailwindcss/plugin')
 
 
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 
-	theme: {
-		extend: {
-			colors: {
-				'mygray': {
-					'50': '#f6f7f8',
-					'100': '#ebecee',
-					'200': '#dfe1e5', 
-					'300': '#c2c7ce',
-					'400': '#a5aab5',
-					'500': '#8f94a2',
-					'600': '#7d8293',
-					'700': '#717484',
-					'800': '#5f606e',
-					'900': '#4e505a',
-					'950': '#323339',
-				},
-			},
-		},
-	},
+  theme: {
+    extend: {
+      colors: {
+        'mygray': {
+          '50': '#f6f7f8',
+          '100': '#ebecee',
+          '200': '#dfe1e5',
+          '300': '#c2c7ce',
+          '400': '#a5aab5',
+          '500': '#8f94a2',
+          '600': '#7d8293',
+          '700': '#717484',
+          '800': '#5f606e',
+          '900': '#4e505a',
+          '950': '#323339',
+        },
+      },
+    },
+  },
 }
