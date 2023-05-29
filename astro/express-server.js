1259d6e692cabb1f715491c846a82367f62707a5
Dario Guarascio <dario.guarascio@gmail.com>
2023-05-29 15:29:34 +0200

    enabled static file server for public data

--- astro/express-server.js
+++ astro/express-server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import morgan from 'morgan'
 import { handler as astroSsrHandler } from './dist/server/entry.mjs';
+import serveIndex from 'serve-index'
 const __dirname = import.meta.url.slice(7, import.meta.url.lastIndexOf("/"));
 
 const app = express();
@@ -9,6 +10,13 @@ app.use(morgan('combined'))
 
 app.use(express.static('./dist/client/'))
 app.use('/tmp', express.static('./public/tmp/'))
+
+
+// Serve URLs like /ftp/thing as public/ftp/thing
+// The express.static serves the file contents
+// The serveIndex is this module serving the directory
+app.use('/exports', express.static('/data'), serveIndex('/data', {'icons': true}))
+// app.use('/ftp', express.static('public/ftp'), serveIndex('public/ftp', {'icons': true}))
 
 
 app.use(astroSsrHandler);
