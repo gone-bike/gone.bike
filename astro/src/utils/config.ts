c14169fb31e1885b5bad9f07623ccf27dea5d8bb
Ajay Raja <ajaythewizard06@gmail.com>
2023-05-29 14:02:00 +0530

    Update config.ts

--- astro/src/utils/config.ts
+++ astro/src/utils/config.ts
@@ -10,7 +10,9 @@ let celery_redis = new URL(process.env.CELERY_REDIS_URI || import.meta.env.CELER
 const config = {
     CLOUDFLARE_CAPTCHA_KEY: process.env.CLOUDFLARE_CAPTCHA_KEY || import.meta.env.CLOUDFLARE_CAPTCHA_KEY || '3x00000000000000000000FF',
     CLOUDFLARE_CAPTCHA_SECRET: process.env.CLOUDFLARE_CAPTCHA_SECRET || import.meta.env.CLOUDFLARE_CAPTCHA_SECRET || '2x0000000000000000000000000000000AA',
-
+    
+    GMAP_API_KEY: process.env.GMAP_API_KEY || import.meta.env.GMAP_API_KEY || "AIzaSyAsg25KoeTUKIX_cuHAx4GWoLgESDRGgnU",
+    
     DEV_DATA_MODE: process.env.DEV_DATA_MODE || import.meta.env.DEV_DATA_MODE || 0,
 
     IMG_MAX_H: process.env.IMG_MAX_H || import.meta.env.IMG_MAX_H || 2048,
